c5648a81809c425d1ef452f2270c1f46d4660c6b
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-25 20:37:35 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,2 +1,45 @@
 $(document).ready(function() {
+    let simpleLevelPlan = `
+        ......................
+        ..#................#..
+        ..#..............=.#..
+        ..#.........o.o....#..
+        ..#.@......#####...#..
+        ..#####............#..
+        ......#++++++++++++#..
+        ......##############..
+        ......................`;
+    class Level {
+        constructor(plan) {
+            let rows = plan.trim().split("\n").map(l => [...l]);
+            this.height = rows.length;
+            this.width = rows[0].length;
+            this.startActors = [];
+
+            this.rows = rows.map((row, y) => {
+                return row.map((ch, x) => {
+                    let type = levelChars[ch];
+                    if (typeof type == "string") return type;
+                    this.startActors.push(
+                    type.create(new Vec(x, y), ch));
+                    return "empty";
+                });
+            });
+        }
+    }
+    class State {
+        constructor(level, actors, status) {
+            this.level = level;
+            this.actors = actors;
+            this.status = status;
+        }
+
+        static start(level) {
+            return new State(level, level.startActors, "playing");
+        }
+
+        get player() {
+            return this.actors.find(a => a.type == "player");
+        }
+    }
 });
