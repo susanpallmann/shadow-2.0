da0893dee1378416bced71f52b5494d5988eb63e
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-26 18:53:31 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
     
-    var childPos = $('#character').offset();
-    var parentPos = $('#character').parent().offset();
-    var childOffset = {
-        top: childPos.top - parentPos.top,
-        left: childPos.left - parentPos.left
+    let charWidth = $('#character').width();
+    let charHeight = $('#character').height();
+    let childPos = $('#character').offset();
+    let parentPos = $('#character').parent().offset();
+    let childOffset = {
+        top: childPos.top + charWidth/2 - parentPos.top,
+        left: childPos.left + charHeight/2 - parentPos.left
     }
     console.log(childOffset.top);
     console.log(childOffset.left);
 
     function updateScroll(positionLeft, positionTop) {
+        
+        let charWidth = $('#character').width();
+        let charHeight = $('#character').height();
+        
         
     }
     
