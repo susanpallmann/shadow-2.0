d4fcab6802a0b23c5bc1b55ce1609c456ed5f293
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-26 19:22:09 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     let childPos = $('#character').offset();
     let parentPos = $('#character').parent().offset();
     let childOffset = {
-        top: (childPos.top - charWidth - parentPos.top)/$('#game').height()*2,
-        left: (childPos.left - charHeight - parentPos.left)/$('#game').width()*2
+        top: (childPos.top + charWidth/2 - parentPos.top)/$('#game').height()*2,
+        left: (childPos.left + charHeight/2 - parentPos.left)/$('#game').width()*2
     }
     console.log(childOffset.top);
     console.log(childOffset.left);
