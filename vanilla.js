ec2598339d15126482b8f0ba75b7f826d704bf9d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-26 18:46:07 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     
-    
-    
+    var childPos = $('#character').offset();
+    var parentPos = $('#character').parent().offset();
+    var childOffset = {
+        top: childPos.top - parentPos.top,
+        left: childPos.left - parentPos.left
+    }
+
 });
