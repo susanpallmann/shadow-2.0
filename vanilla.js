ce0d040c391a0ac3968094050a73f6e465a66dfd
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-27 17:09:36 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -27,6 +27,131 @@ $(document).ready(function() {
         ......##############..
         ......................`;
     
+    // Class to store 2-dimensional values, position and size of actors
+    class Vec {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        // Method to add new x/y values to the current ones to reuse as needed
+        // TODO: I'm guessing this method requires an object as its argument?
+        plus(other) {
+            return new Vec(this.x + other.x, this.y + other.y);
+        }
+        
+        // Method to multiply a factor value to the current ones to reuse as needed
+        // Note this applies the same factor to both x and y
+        times(factor) {
+            return new Vec(this.x * factor, this.y * factor);
+        }
+    }
+    
+    // Class to store player actor
+    class Player {
+        constructor(pos, speed) {
+            
+            // Has position and speed properties
+            // TODO: May want to add more properties down the road with evolution in mind,
+            //       if this is the right place to do so?
+            this.pos = pos;
+            this.speed = speed;
+        }
+        
+        // This answers the level constructor's check for type
+        // TODO: I'm guessing this will be in all element class constructors
+        get type() { 
+            return "player";
+        }
+        
+        // TODO: I think this creates the player when the game is first started and isn't used again
+        // Initial position is set to be 1/2 square unit above its starting place because the character 
+        // is 1.5 units tall
+        static create(pos) {
+            return new Player(pos.plus(new Vec(0, -0.5)),
+                              new Vec(0, 0));
+        }
+    }
+    
+    // Since size isn't changing, we're storing this on the prototype rather than the instance
+    // TODO: May need to change this if game mechanics decide to change size
+    Player.prototype.size = new Vec(0.8, 1.5);
+    
+    // Class to store lava actors (keeping in mind they can behave in three different ways
+    class Lava {
+        constructor(pos, speed, reset) {
+            this.pos = pos;
+            this.speed = speed;
+            this.reset = reset;
+        }
+        
+        get type() {
+            return "lava";
+        }
+        
+        // Create sends parameters to constructor differently depending on which type of lava we're creating
+        static create(pos, ch) {
+            
+            // Horizontally moving lava
+            if (ch == "=") {
+                
+                // Horizontal speed (2, 0)
+                return new Lava(pos, new Vec(2, 0));
+                
+            // Vertically moving lava that bounces
+            } else if (ch == "|") {
+                
+                // Vertical speed (0, 2)
+                return new Lava(pos, new Vec(0, 2));
+            
+            // Vertically moving lava that resets when it hits a solid block
+            } else if (ch == "v") {
+                
+                // This lava has a reset point, pos
+                // Vertical speed (0, 3)
+                return new Lava(pos, new Vec(0, 3), pos);
+            }
+        }
+    }
+    
+    // Like the player, size doesn't need to be stored on the instance since it shouldn't be changing
+    Lava.prototype.size = new Vec(1,1);
+    
+    // Class to store coin actors, now with more math!
+    class Coin {
+        
+        // Constructor has a basePos to aid with wobbling
+        constructor(pos, basePos, wobble) {
+            this.pos = pos;
+            this.basePos = basePos;
+            this.wobble = wobble;
+        }
+        
+        get type() {
+            return "coin";
+        }
+        
+        static create(pos) {
+            
+            let basePos = pos.plus(new Vec(0.2), 0.1));
+            
+            // Okay I want to comment this one, but I just trust the dude's math
+            // Random is to avoid coins moving in sync
+            return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+        }
+    }
+    
+    // Size doesn't change, storing in prototype rather than instance
+    Coin.prototype.size = new Vec(0.6, 0.6);
+    
+    // levelChars object maps plan characters to their respective roles
+    // It's important that background items are strings, and actors are not
+    const levelChars = {
+        ".": "empty", "#": "wall", "+": "lava",
+        "@": Player, "o": Coin,
+        "=": Lava, "|": Lava, "v": Lava
+    };
+    
     // Stores a level object, using a string like the above as the argument to be read
     class Level {
         constructor(plan) {
@@ -34,7 +159,7 @@ $(document).ready(function() {
             // Trims whitespace before/after plan, then splits on new line (\n) to create rows
             // Maps these rows to create arrays of characters
             // Rows then holds an array of arrays of characters; it is all of the rows
-            let rows = plan.trim().split('\n').map(l => [...l]);
+            let rows = plan.trim().split("\n").map(l => [...l]);
             
             // Calculate height of the map by determining how many individual row arrays were stored in the rows variable
             this.height = rows.length;
@@ -89,4 +214,75 @@ $(document).ready(function() {
         }
     }
     
+    // How we create a Level instance (this doesn't display anything, and currently time/motion don't exist)
+    let simpleLevel = new Level(simpleLevelPlan);
+    console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+    
+    // Helper function to create element with some given parameters
+    // TODO: Rename this, don't like "elt"
+    function elt(name, attrs, ...children) {
+        
+        let dom = document.createElement(name);
+        
+        // Loops through attributes provided and adds them as DOM attributes
+        for (let attr of Object.keys(attrs)) {
+            dom.setAttribute(attr, attrs[attr]);
+        }
+        
+        // Loops through children and appends them to the element we're creating
+        for (let child of children) {
+            dom.appendChild(child);
+        }
+        
+        // Returns created and customized HTML element
+        return dom;
+    }
+    
+    // Scale of one in-game unit in pixels
+    const scale = 64;
+    
+    // Creating the background grid from level object
+    // We're drawing a table since our maps are grid-based
+    function drawGrid(level) {
+        
+        // TODO: elt is here
+        // The table is given a background class and width is set to the width of the level times our 
+        // scale from earlier
+        // I would normally avoid inline styles at all costs, but I'd like game scale to stay centralized,
+        // so it can live in javaScript in this case only
+        return elt("table", {
+            class: "background",
+            style: `width: ${level.width * scale}px`
+            
+        // Its children are the rows, mapped to be table row elements (makes sense, right?), with heights 
+        // of the scale
+        }, ...level.rows.map(row =>
+           elt("tr", {style: `height: ${scale}px`},
+               
+               // The children of the row arrays are mapped to td elements
+               // TODO: I assume these will have the correct width automatically
+               // Each is given a class that matches their type, easy styling for CSS :)
+               ...row.map(type => elt("td", {class: type})))
+        ));
+    }
+    
+    
+    // Creates display, when given an element to be appended to and a level object]
+    class DOMDisplay {
+        constructor(parent, level) {
+            
+            // Calls method to draw grid, this is only generated once since it doesn't change
+            // TODO: elt is here too, whenever you change it
+            this.dom =  elt("div", {class: "game"}, drawGrid(level));
+            
+            // Used to track element holding actors so they can be removed/replaced
+            this.actorLayer = null;
+            parent.appendChild(this.dom);
+        }
+        
+        // Removes display
+        clear() { 
+            this.dom.remove();
+         }
+    }
 });
