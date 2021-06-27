a513937ff2375cf7ba8ec34c759833ea5b444220
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-26 21:48:55 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -53,6 +53,40 @@ $(document).ready(function() {
     // When player presses "D" key
     // Check if this movement would cause a collision
     // If not, step
+    
+
+});
+
+    function updateScroll() {
+        
+        // Get height/width of player (#character)
+        let charWidth = $('#character')[0].getBoundingClientRect().width;
+        let charHeight = $('#character')[0].getBoundingClientRect().height;
+        
+        // Get position of player
+        let childPos = $('#character').offset();
+        
+        // Get position of game element (#game)
+        let parentPos = $('#character').parent().offset();
+        
+        // Math to work out precisely how far the center of the player is from the top-left corner of the #game element
+        // End result is a fraction (should be less than 1 and more than 0)
+        let childOffset = {
+            top: (childPos.top + charHeight/2 - parentPos.top)/$('#game')[0].getBoundingClientRect().height,
+            left: (childPos.left + charWidth/2 - parentPos.left)/$('#game')[0].getBoundingClientRect().width
+        }
+        
+        // Math to translate fraction from previous step to a scale of 100 to -100 for use translating #game element
+        // -100 range is further right/down, 100 is left/up (weird, but it works, so)
+        var newLeft = ((childOffset.left*200)-100)*-1;
+        var newTop = ((childOffset.top*200)-100)*-1;
+        
+        // Updates element CSS to reflect the new position, centering the player within the #canvas viewport
+        $('#game').css('left', newLeft + '%');
+        $('#game').css('top', newTop + '%');
+        
+    }
+
     $(document).bind('keydown', function(e) {
         console.log('key pressed');
         if (e.keyCode == 68) {
@@ -70,5 +104,3 @@ $(document).ready(function() {
         }
         return false;
     });
-
-});
