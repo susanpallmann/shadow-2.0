920d77bde775bbdec7b89a13cbda7b37ee46555b
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-06-26 21:12:19 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
     
-    let charWidth = $('#character')[0].getBoundingClientRect().width;
-    let charHeight = $('#character')[0].getBoundingClientRect().height;
-    let childPos = $('#character').offset();
-    let parentPos = $('#character').parent().offset();
-    let childOffset = {
-        top: (childPos.top + charHeight/2 - parentPos.top)/$('#game')[0].getBoundingClientRect().height,
-        left: (childPos.left + charWidth/2 - parentPos.left)/$('#game')[0].getBoundingClientRect().width
-    }
-    console.log(childOffset.top*100);
-    console.log(childOffset.left*100);
-
+    // Below focuses view on where character currently is by adjusting the position of the game element within the canvas element
     function updateScroll(positionLeft, positionTop) {
+        
+        let charWidth = $('#character')[0].getBoundingClientRect().width;
+        let charHeight = $('#character')[0].getBoundingClientRect().height;
+        let childPos = $('#character').offset();
+        let parentPos = $('#character').parent().offset();
+        let childOffset = {
+            top: (childPos.top + charHeight/2 - parentPos.top)/$('#game')[0].getBoundingClientRect().height,
+            left: (childPos.left + charWidth/2 - parentPos.left)/$('#game')[0].getBoundingClientRect().width
+        }
         
         var newLeft = ((positionLeft*200)-100)*-1;
         var newTop = ((positionTop*200)-100)*-1;
@@ -22,5 +21,9 @@ $(document).ready(function() {
     }
     
     updateScroll(childOffset.left, childOffset.top);
+    
+    $(window).resize(function() {
+        updateScroll(childOffset.left, childOffset.top);
+    }
     
 });
